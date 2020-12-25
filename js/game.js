cde9e7ee9bc6ba83b634ad9c096e44b44a8d54af
Selim Şeker <selim.seker00@gmail.com>
2020-12-25 15:42:02 +0300

    new maze and maze2matrix

--- js/game.js
+++ js/game.js
@@ -1,5 +1,6 @@
 import { ColladaLoader } from "../dependencies/ColladaLoader.js";
 
+localizer(1);
 var scene = new THREE.Scene();
 let ball;
 let maze;
@@ -56,7 +57,7 @@ let shape;
 const loader = new ColladaLoader(loadingManager);
 loader.load("../models/sphere.dae", function (collada) {
   collada.scene.position.x += 0;
-  collada.scene.position.y += 1;
+  collada.scene.position.y += 0.75;
   collada.scene.position.z += 0;
   ball = collada.scene;
 });
@@ -80,6 +81,10 @@ var render = function () {
   renderer.render(scene, camera);
 };
 
+var old_mazeX = 0;
+var old_mazeY = 0;
+var old_symbol = ".";
+
 // Run game loop(update, render, repeat)
 var GameLoop = function () {
   requestAnimationFrame(GameLoop);
@@ -87,21 +92,34 @@ var GameLoop = function () {
     e = e || window.event;
     switch (e.key) {
       case "w":
-        ball.position.x += 0.5;
-        camera.position.x += 0.5;
+        ball.position.x += 0.25;
+        camera.position.x += 0.25;
         break;
       case "s":
-        ball.position.x -= 0.5;
-        camera.position.x -= 0.5;
+        ball.position.x -= 0.25;
+        camera.position.x -= 0.25;
         break;
       case "a":
-        ball.position.z -= 0.5;
-        camera.position.z -= 0.5;
+        ball.position.z -= 0.25;
+        camera.position.z -= 0.25;
         break;
       case "d":
-        ball.position.z += 0.5;
-        camera.position.z += 0.5;
+        ball.position.z += 0.25;
+        camera.position.z += 0.25;
         break;
+    }
+
+    // calculate the new position in the maze
+    // if it is changed then update the maze_matrix
+    var new_mazePosition = localizer(ball.position.x, ball.position.z);
+    if (old_mazeX != new_mazePosition.x || old_mazeY != new_mazePosition.z) {
+      maze_matrix[old_mazeY][old_mazeX] = old_symbol;
+      old_symbol = maze_matrix[new_mazePosition.z][new_mazePosition.x];
+      maze_matrix[new_mazePosition.z][new_mazePosition.x] = "O";
+      old_mazeX = new_mazePosition.x;
+      old_mazeY = new_mazePosition.z;
+      console.clear();
+      print_mazeMat();
     }
   };
   update();
