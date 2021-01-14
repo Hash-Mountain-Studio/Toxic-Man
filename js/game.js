df32222b7d2b5cf4fa13fc7cb5bff836aaa85acd
Yusuf Keten <ketenyusuf@gmail.com>
2021-01-14 07:54:31 +0300

    Collision system first version is created

--- js/game.js
+++ js/game.js
@@ -1,7 +1,12 @@
 import { ColladaLoader } from "../dependencies/ColladaLoader.js";
 import { Geometry } from "../dependencies/three.module.js";
-import { gameCondition, radius, updateCameraInStart, degToRad } from "./startMenu.js";
-
+import {
+    gameCondition,
+    radius,
+    updateCameraInStart,
+    degToRad,
+} from "./startMenu.js";
+import * as dat from "../dependencies/dat.gui.module.js";
 var scene = new THREE.Scene();
 let ball;
 let ghost1;
@@ -23,12 +28,11 @@ camera.position.set(-15, 0, -0.035);
 // let controls = new THREE.OrbitControls(camera, renderer.domElement);
 // controls.update();
 
-
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-window.addEventListener("resize", function () {
+window.addEventListener("resize", function() {
     var width = window.innerWidth;
     var height = window.innerHeight;
     renderer.setSize(width, height);
@@ -48,20 +52,56 @@ window.addEventListener("resize", function () {
 // plane.position.y = 0.001;
 var cubegeo = new THREE.CubeGeometry(1000, 1000, 1000);
 var cube_materials = [
-    new THREE.MeshBasicMaterial ({ map: new THREE.TextureLoader().load( 'images/skybox_space/ft.png' ), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(  'images/skybox_space/bk.png' ), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(  'images/skybox_space/up.png' ), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(  'images/skybox_space/dn.png' ), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(  'images/skybox_space/rt.png' ), side: THREE.DoubleSide}),
-    new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load(  'images/skybox_space/lf.png' ), side: THREE.DoubleSide}),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/ft.png"),
+        side: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/bk.png"),
+        side: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/up.png"),
+        side: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/dn.png"),
+        side: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/rt.png"),
+        side: THREE.DoubleSide,
+    }),
+    new THREE.MeshBasicMaterial({
+        map: new THREE.TextureLoader().load("images/skybox_space/lf.png"),
+        side: THREE.DoubleSide,
+    }),
 ];
 
 var cubeMaterial = new THREE.MeshFaceMaterial(cube_materials);
 var cube = new THREE.Mesh(cubegeo, cubeMaterial);
 
+var sliderOptions = {
+    xSlider: 0,
+    ySlider: 0,
+    camx: 0,
+    camy: 0,
+};
+
+var gui = new dat.GUI();
+var xy = gui.addFolder("Position");
+xy.add(sliderOptions, "xSlider", -3, 5).listen();
+xy.add(sliderOptions, "ySlider", -15, 5).listen();
+
+xy.open();
+
+var camxy = gui.addFolder("Camera Position");
+camxy.add(sliderOptions, "camx", -0.2, 0.2).name("X").listen();
+camxy.add(sliderOptions, "camy", -0.2, 0.2).name("Y").listen();
+camxy.open();
 
 // COLLADA BEGINS
-const loadingManager = new THREE.LoadingManager(function () {
+const loadingManager = new THREE.LoadingManager(function() {
     scene.add(ball);
     scene.add(ghost1);
     scene.add(ghost2);
@@ -71,7 +111,7 @@ const loadingManager = new THREE.LoadingManager(function () {
 });
 
 const loader = new ColladaLoader(loadingManager);
-loader.load("models/sphere.dae", function (collada) {
+loader.load("models/sphere.dae", function(collada) {
     collada.scene.position.x += 0;
     collada.scene.position.y += 0.5;
     collada.scene.position.z += 0;
@@ -79,7 +119,7 @@ loader.load("models/sphere.dae", function (collada) {
     BallObject = new Movable(ball);
 });
 
-loader.load("models/ghost.dae", function (collada) {
+loader.load("models/ghost.dae", function(collada) {
     collada.scene.position.x += 14;
     collada.scene.position.y += 0.75;
     collada.scene.position.z += -14;
@@ -87,7 +127,7 @@ loader.load("models/ghost.dae", function (collada) {
     GhostObject = new Movable(ghost1);
 });
 
-loader.load("models/ghost2.dae", function (collada) {
+loader.load("models/ghost2.dae", function(collada) {
     collada.scene.position.x += 14;
     collada.scene.position.y += 0.75;
     collada.scene.position.z += 14;
@@ -95,12 +135,11 @@ loader.load("models/ghost2.dae", function (collada) {
     GhostObject2 = new Movable(ghost2);
 });
 
-loader.load("models/pacman_maze_cubes.dae", function (collada) {
+loader.load("models/pacman_maze_cubes.dae", function(collada) {
     collada.scene.position.y += 0;
     maze = collada.scene;
 });
 let maze_mat = new Maze();
-
 
 // barrel
 var barrels = [];
@@ -110,40 +149,38 @@ for (var vertex of maze_mat.graph.getVertices()) {
         continue;
     }
     let worldLoc = maze_mat.mazeLoc2worldLoc(vertex);
-    loader.load("models/barrel.dae", function (collada) {
+    loader.load("models/barrel.dae", function(collada) {
         collada.scene.position.x += worldLoc.x;
         collada.scene.position.y += 0;
         collada.scene.position.z += worldLoc.z;
         barrels.push(collada.scene);
-        scene.add(collada.scene)
+        scene.add(collada.scene);
     });
 }
 // COLLADA END
 
-const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-    hemiLight.position.set( 0, 200, 0 );
-    scene.add( hemiLight );
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+hemiLight.position.set(0, 200, 0);
+scene.add(hemiLight);
 
-    const dirLight = new THREE.DirectionalLight( 0xffffff );
-    dirLight.position.set( 0, 200, 100 );
-    dirLight.castShadow = true;
-    dirLight.shadow.camera.top = 180;
-    dirLight.shadow.camera.bottom = - 100;
-    dirLight.shadow.camera.left = - 120;
-    dirLight.shadow.camera.right = 120;
-    scene.add( dirLight );
+const dirLight = new THREE.DirectionalLight(0xffffff);
+dirLight.position.set(0, 200, 100);
+dirLight.castShadow = true;
+dirLight.shadow.camera.top = 180;
+dirLight.shadow.camera.bottom = -100;
+dirLight.shadow.camera.left = -120;
+dirLight.shadow.camera.right = 120;
+scene.add(dirLight);
 
 // Game Logic
-var update = function () {};
+var update = function() {};
 
 //  Draw the scene
 
-var render = function () {
+var render = function() {
     renderer.render(scene, camera);
     updateCameraInStart(camera, ball);
-    if(gameCondition === 4){
-        
-    }
+    if (gameCondition === 4) {}
 };
 
 let isPressW = false;
@@ -151,7 +188,7 @@ let isPressS = false;
 let isPressA = false;
 let isPressD = false;
 
-document.addEventListener("keydown", function (e) {
+document.addEventListener("keydown", function(e) {
     switch (e.key) {
         case "w":
         case "W":
@@ -172,19 +209,23 @@ document.addEventListener("keydown", function (e) {
         case "x":
         case "X":
             // call the green ghost
-            GhostObject2.path = maze_mat.graph.shortest_path(GhostObject2.get_mazeCoord(maze_mat), BallObject.get_mazeCoord(maze_mat));
+            GhostObject2.path = maze_mat.graph.shortest_path(
+                GhostObject2.get_mazeCoord(maze_mat),
+                BallObject.get_mazeCoord(maze_mat)
+            );
             break;
         case "z":
         case "Z":
             // call the red ghost
-            GhostObject.path = maze_mat.graph.shortest_path(GhostObject.get_mazeCoord(maze_mat), BallObject.get_mazeCoord(maze_mat));
+            GhostObject.path = maze_mat.graph.shortest_path(
+                GhostObject.get_mazeCoord(maze_mat),
+                BallObject.get_mazeCoord(maze_mat)
+            );
             break;
-
-        
     }
 });
 
-document.addEventListener("keyup", function (e) {
+document.addEventListener("keyup", function(e) {
     switch (e.key) {
         case "w":
         case "W":
@@ -214,109 +255,179 @@ let current_radius = 3.6;
 // Run game loop(update, render, repeat)
 let canvas = document.body;
 
-canvas.requestPointerLock = canvas.requestPointerLock ||
-                            canvas.mozRequestPointerLock;
+canvas.requestPointerLock =
+    canvas.requestPointerLock || canvas.mozRequestPointerLock;
 
-document.exitPointerLock = document.exitPointerLock ||
-                           document.mozExitPointerLock;
+document.exitPointerLock =
+    document.exitPointerLock || document.mozExitPointerLock;
 
 let isActiveE = 0;
 
-document.onkeypress = function (e) {
-    if((e.key === "e" || e.key === "E") && gameCondition === 2){
-        if(!isActiveE){
+document.onkeypress = function(e) {
+    if ((e.key === "e" || e.key === "E") && gameCondition === 2) {
+        if (!isActiveE) {
             canvas.requestPointerLock();
             isActiveE = 1;
-        }
-        else{
+        } else {
             document.exitPointerLock();
             isActiveE = 0;
         }
     }
 };
 
-document.addEventListener('pointerlockchange', lockChangeAlert, false);
-document.addEventListener('mozpointerlockchange', lockChangeAlert, false);
+document.addEventListener("pointerlockchange", lockChangeAlert, false);
+document.addEventListener("mozpointerlockchange", lockChangeAlert, false);
 
 function lockChangeAlert() {
-    if (document.pointerLockElement === canvas ||
-        document.mozPointerLockElement === canvas) {
-      document.addEventListener("mousemove", updateCamera, false);
+    if (
+        document.pointerLockElement === canvas ||
+        document.mozPointerLockElement === canvas
+    ) {
+        document.addEventListener("mousemove", updateCamera, false);
     } else {
-      document.removeEventListener("mousemove", updateCamera, false);
+        document.removeEventListener("mousemove", updateCamera, false);
     }
 }
 
 function updateCamera(e) {
-    if(gameCondition === 2){
+    if (gameCondition === 2) {
         rotation_angleX += e.movementX * 0.05;
-        if(rotation_angleY >= 20 && rotation_angleY <= 60){
+        if (rotation_angleY >= 20 && rotation_angleY <= 60) {
             rotation_angleY += e.movementY * 0.05;
             current_radius += e.movementY * 0.001;
         }
-        if(rotation_angleY < 30){
+        if (rotation_angleY < 30) {
             rotation_angleY = 30;
             current_radius = 3.14;
         }
-        if(rotation_angleY > 60){
+        if (rotation_angleY > 60) {
             rotation_angleY = 60;
             current_radius = 3.74;
         }
     }
-    
-    
+
     //eyeY += e.movementY * 0.005;
 }
 
 // shortest path from ghost's locaction to pacman's location
+function collisionCheckSystemWithMachineLearningAndDeepLearning() {
+    // Sphere Coordinates
+    let sx = ball.position.x;
+    let sy = ball.position.y;
+    let sz = -ball.position.z;
+    let radius = 0.4;
+    console.log(sx + " " + sy + " " + sz);
+    for (let i = 0; i < 150; i++) {
+        console.log(i);
+        if (maze["children"][i]["name"].substr(0, 5) != "Cube.") continue;
 
-var GameLoop = function () {
+        let currentCube = maze["children"][i];
+        // Cube Coordinates
+        let cx = currentCube["position"]["x"];
+        let cz = currentCube["position"]["y"];
+        let cy = currentCube["position"]["z"];
+
+        if (
+            cx - 1 - radius < sx &&
+            sx <= cx + 1 + radius &&
+            cz - 1 - radius <= sz &&
+            sz <= cz + 1 + radius
+        ) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function moveForward() {
+    BallObject.speedUp(
+        speed * Math.cos(degToRad(rotation_angleX)),
+        0,
+        speed * Math.sin(degToRad(rotation_angleX))
+    );
+    BallObject.rotation(0, 0, -angleIncrement);
+    BallObject.action();
+    BallObject.resetMotion();
+}
+
+function moveBackward() {
+    BallObject.speedUp(-speed * Math.cos(degToRad(rotation_angleX)),
+        0, -speed * Math.sin(degToRad(rotation_angleX))
+    );
+    BallObject.rotation(0, 0, angleIncrement);
+    BallObject.action();
+    BallObject.resetMotion();
+}
+
+function moveLeft() {
+    BallObject.speedUp(
+        speed * Math.sin(degToRad(rotation_angleX)),
+        0, -speed * Math.cos(degToRad(rotation_angleX))
+    );
+    BallObject.rotation(-angleIncrement, 0, 0);
+    BallObject.action();
+    BallObject.resetMotion();
+}
+
+function moveRight() {
+    BallObject.speedUp(-speed * Math.sin(degToRad(rotation_angleX)),
+        0,
+        speed * Math.cos(degToRad(rotation_angleX))
+    );
+    BallObject.rotation(angleIncrement, 0, 0);
+    BallObject.action();
+    BallObject.resetMotion();
+}
+var GameLoop = function() {
     requestAnimationFrame(GameLoop);
+
     if (gameCondition === 2 && ball) {
-        if (isPressW) {
+        if (collisionCheckSystemWithMachineLearningAndDeepLearning() == false) {
+            if (isPressW) {
+                moveForward();
+                if (collisionCheckSystemWithMachineLearningAndDeepLearning() == true)
+                    moveBackward();
+                // ball.position.x += speed * Math.cos(degToRad(rotation_angleX));
+                // ball.position.z += speed * Math.sin(degToRad(rotation_angleX));
 
-            BallObject.speedUp(speed * Math.cos(degToRad(rotation_angleX)), 0, speed * Math.sin(degToRad(rotation_angleX)));
-            BallObject.rotation(0, 0, -angleIncrement);
-            // ball.position.x += speed * Math.cos(degToRad(rotation_angleX));
-            // ball.position.z += speed * Math.sin(degToRad(rotation_angleX));
+                // angleX += angleIncrement;
+                // ball.rotation.y = angleX * (Math.PI / 180);
+            }
+            if (isPressS) {
+                moveBackward();
+                if (collisionCheckSystemWithMachineLearningAndDeepLearning() == true)
+                    moveForward();
+                // ball.position.x -= speed * Math.cos(degToRad(rotation_angleX));
+                // ball.position.z -= speed * Math.sin(degToRad(rotation_angleX));
 
-            // angleX += angleIncrement;
-            // ball.rotation.y = angleX * (Math.PI / 180);
+                // angleX -= angleIncrement;
+                // ball.rotation.y = angleX * (Math.PI / 180);
+            }
+            if (isPressA) {
+                moveLeft();
+                if (collisionCheckSystemWithMachineLearningAndDeepLearning() == true)
+                    moveRight();
+                // ball.position.x += speed * Math.sin(degToRad(rotation_angleX));
+                // ball.position.z -= speed * Math.cos(degToRad(rotation_angleX));
+
+                // angleY -= angleIncrement;
+                // ball.rotation.x = angleY * (Math.PI / 180);
+            }
+            if (isPressD) {
+                moveRight();
+                if (collisionCheckSystemWithMachineLearningAndDeepLearning() == true)
+                    moveLeft();
+
+                // ball.position.x -= speed * Math.sin(degToRad(rotation_angleX));
+                // ball.position.z += speed * Math.cos(degToRad(rotation_angleX));
+
+                // angleY += angleIncrement;
+                // ball.rotation.x = angleY * (Math.PI / 180);
+            }
         }
-        if (isPressS) {
-            BallObject.speedUp(-speed * Math.cos(degToRad(rotation_angleX)), 0, -speed * Math.sin(degToRad(rotation_angleX)));
-            BallObject.rotation(0, 0, angleIncrement);
-            // ball.position.x -= speed * Math.cos(degToRad(rotation_angleX));
-            // ball.position.z -= speed * Math.sin(degToRad(rotation_angleX));
-
-            // angleX -= angleIncrement;
-            // ball.rotation.y = angleX * (Math.PI / 180);
-        }
-        if (isPressA) {
-            BallObject.speedUp(speed * Math.sin(degToRad(rotation_angleX)), 0, -speed * Math.cos(degToRad(rotation_angleX)));
-            BallObject.rotation(-angleIncrement, 0, 0);
-
-            // ball.position.x += speed * Math.sin(degToRad(rotation_angleX));
-            // ball.position.z -= speed * Math.cos(degToRad(rotation_angleX));
-
-            // angleY -= angleIncrement;
-            // ball.rotation.x = angleY * (Math.PI / 180);
-        }
-        if (isPressD) {
-            BallObject.speedUp(-speed * Math.sin(degToRad(rotation_angleX)), 0, speed * Math.cos(degToRad(rotation_angleX)));
-            BallObject.rotation(angleIncrement, 0, 0);
-
-            // ball.position.x -= speed * Math.sin(degToRad(rotation_angleX));
-            // ball.position.z += speed * Math.cos(degToRad(rotation_angleX));
-
-            // angleY += angleIncrement;
-            // ball.rotation.x = angleY * (Math.PI / 180);
-        }
-
-
         GhostObject.execute_thePath();
         GhostObject2.execute_thePath();
-
 
         BallObject.action();
         BallObject.resetMotion();
@@ -324,10 +435,13 @@ var GameLoop = function () {
         GhostObject.resetMotion();
         GhostObject2.action();
         GhostObject2.resetMotion();
-    
-        camera.position.x = ball.position.x - current_radius * Math.cos(degToRad(rotation_angleX));
-        camera.position.z = ball.position.z - current_radius * Math.sin(degToRad(rotation_angleX));
-        camera.position.y = ball.position.y + current_radius * Math.sin(degToRad(rotation_angleY));
+
+        camera.position.x =
+            ball.position.x - current_radius * Math.cos(degToRad(rotation_angleX));
+        camera.position.z =
+            ball.position.z - current_radius * Math.sin(degToRad(rotation_angleX));
+        camera.position.y =
+            ball.position.y + current_radius * Math.sin(degToRad(rotation_angleY));
         camera.lookAt(ball.position.x, ball.position.y, ball.position.z);
         // corner check
         //let corner = maze_mat.corner_check(ball.position.x, ball.position.z);
--- js/movable_objects.js
+++ js/movable_objects.js
@@ -1,140 +1,146 @@
 class Movable {
-    constructor(object){
+    constructor(object) {
         this.object = object;
-        this.speed = {x:0, y:0, z:0};
-        this.angle = {x:0, y:0, z:0};
-        this.old_position = {x:object.position.x, y:object.position.y, z:object.position.z};
+        this.speed = { x: 0, y: 0, z: 0 };
+        this.angle = { x: 0, y: 0, z: 0 };
+        this.old_position = {
+            x: object.position.x,
+            y: object.position.y,
+            z: object.position.z,
+        };
         this.auto_speed = 0.2;
         this.isCommand_continue = false;
         this.path;
     }
 
-    action(){
+    action() {
         // translation
         this.object.position.x += this.speed.x;
         this.object.position.z += this.speed.z;
 
-        this.object.position.x = Math.round(this.object.position.x*100)/100;
-        this.object.position.z = Math.round(this.object.position.z*100)/100;
+        this.object.position.x = Math.round(this.object.position.x * 100) / 100;
+        this.object.position.z = Math.round(this.object.position.z * 100) / 100;
 
         // rotation
-        this.object.rotation.z = 2*this.angle.z * (Math.PI / 180);
-        this.object.rotation.x = this.angle.x * (Math.PI / 180);        
+        this.object.rotation.z = 2 * this.angle.z * (Math.PI / 180);
+        this.object.rotation.x = this.angle.x * (Math.PI / 180);
     }
 
     /*
-    this function first moves along x-axis (columns in maze matrix)
-    then moves along z-axis (rows in maze matrix)
-    */
+          this function first moves along x-axis (columns in maze matrix)
+          then moves along z-axis (rows in maze matrix)
+          */
     moveTo(maze, dest_col, dest_row) {
         console.log(this.get_mazeCoord(maze));
     }
 
-    oneStepForward(){
-        if (this.old_position.x+2 == Math.round(this.object.position.x*100)/100 ) {
+    oneStepForward() {
+        if (
+            this.old_position.x + 2 ==
+            Math.round(this.object.position.x * 100) / 100
+        ) {
             this.old_position.x = this.object.position.x;
             // motion is end
             return false;
-        }
-        else{
-            this.speedUp(this.auto_speed, 0,0)
+        } else {
+            this.speedUp(this.auto_speed, 0, 0);
             this.rotation(0, 0, -10);
             return true;
         }
-
     }
 
-    oneStepBackward(){
-        if (this.old_position.x-2 == Math.round(this.object.position.x*100)/100 ) {
+    oneStepBackward() {
+        if (
+            this.old_position.x - 2 ==
+            Math.round(this.object.position.x * 100) / 100
+        ) {
             this.old_position.x = this.object.position.x;
             // motion is end
             return false;
-        }
-        else{
-            this.speedUp(-this.auto_speed, 0,0)
+        } else {
+            this.speedUp(-this.auto_speed, 0, 0);
             this.rotation(0, 0, 10);
             return true;
         }
     }
 
-    oneStepRight(){
-        if (this.old_position.z+2 == Math.round(this.object.position.z*100)/100 ) {
+    oneStepRight() {
+        if (
+            this.old_position.z + 2 ==
+            Math.round(this.object.position.z * 100) / 100
+        ) {
             this.old_position.z = this.object.position.z;
             // motion is end
             return false;
-        }
-        else{
-            this.speedUp(0, 0, this.auto_speed)
+        } else {
+            this.speedUp(0, 0, this.auto_speed);
             this.rotation(10, 0, 0);
             return true;
         }
-        
     }
 
-    oneStepLeft(){
-        if (this.old_position.z-2 == Math.round(this.object.position.z*100)/100 ) {
+    oneStepLeft() {
+        if (
+            this.old_position.z - 2 ==
+            Math.round(this.object.position.z * 100) / 100
+        ) {
             this.old_position.z = this.object.position.z;
             // motion is end
             return false;
-        }
-        else{
-            this.speedUp(0, 0, -this.auto_speed)
+        } else {
+            this.speedUp(0, 0, -this.auto_speed);
             this.rotation(-10, 0, 0);
             return true;
         }
-        
     }
 
-    execute_thePath(){
+    execute_thePath() {
         if (this.path == undefined || this.path == null) {
             return;
         }
         let command = this.path[0];
         if (command == "forward") {
             this.isCommand_continue = this.oneStepForward();
-        }
-        else if (command == "back") {
-            this.isCommand_continue = this.oneStepBackward();            
-        }
-        else if (command == "left") {
-            this.isCommand_continue = this.oneStepLeft();            
-        }
-        else if (command == "right") {
-            this.isCommand_continue = this.oneStepRight();            
+        } else if (command == "back") {
+            this.isCommand_continue = this.oneStepBackward();
+        } else if (command == "left") {
+            this.isCommand_continue = this.oneStepLeft();
+        } else if (command == "right") {
+            this.isCommand_continue = this.oneStepRight();
         }
         if (!this.isCommand_continue && this.path.length != 0) {
             this.path.shift();
         }
-    
     }
 
-    speedUp(x, y, z){
+    speedUp(x, y, z) {
         this.speed.x += x;
         this.speed.y += y;
         this.speed.z += z;
-        this.speed.x = Math.round(this.speed.x*100)/100;
-        this.speed.y = Math.round(this.speed.y*100)/100;
-        this.speed.z = Math.round(this.speed.z*100)/100;
-
+        this.speed.x = Math.round(this.speed.x * 100) / 100;
+        this.speed.y = Math.round(this.speed.y * 100) / 100;
+        this.speed.z = Math.round(this.speed.z * 100) / 100;
     }
 
-    rotation(x, y, z){
+    rotation(x, y, z) {
         this.angle.x += x;
         this.angle.y += y;
         this.angle.z += z;
+
+        this.object.rotation.x = y * (Math.PI / 180);
+        this.object.rotation.y = x * (Math.PI / 180);
     }
 
-    resetMotion(){
-        this.speed = {x:0, y:0, z:0};
+    resetMotion() {
+        this.speed = { x: 0, y: 0, z: 0 };
     }
 
-    get_worldCoord(){
+    get_worldCoord() {
         return this.object.position;
     }
 
-    get_mazeCoord(maze){
+    get_mazeCoord(maze) {
         let loc = maze.localizer(this.object.position.x, this.object.position.z);
-        return (loc.x-1).toString()+":"+(loc.z-1).toString();
+        return (loc.x - 1).toString() + ":" + (loc.z - 1).toString();
     }
-
 }
